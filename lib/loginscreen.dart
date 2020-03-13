62902c24a854a6ff63f87b0e03991e0a495aec91
Madihah <madihah@gmail.com>
2020-03-13 23:12:59 +0800

    first commit

---
+++ lib/loginscreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+ 
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
